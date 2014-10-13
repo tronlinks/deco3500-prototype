20fd6f8a57f4a6251b9a4c963fae8107293447bf
Aimee Leong <aimeemayleong@gmail.com>
2014-10-13 14:44:30 +1000

    Temp , main saving works

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
+$(function(){
+
+// new GMaps({
+//   div: '#map',
+//   lat: -12.043333,
+//   lng: -77.028333
+// });
+
+
 Ember.Application.initializer({
     name: 'userapp',
     initialize: function(container, application) {
@@ -13,26 +22,6 @@ Ember.Application.initializer({
 
 App = Ember.Application.create();
 
-App.Router.map(function() {
-    this.route('login');
-    this.route('signup');
-    // this.route('index');
-});
-
-App.ApplicationRoute = Ember.Route.extend(Ember.UserApp.ApplicationRouteMixin, {
-
-});
-App.SignupController = Ember.Controller.extend(Ember.UserApp.FormControllerMixin);
-App.LoginController = Ember.Controller.extend(Ember.UserApp.FormControllerMixin);
-App.IndexRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin, {
-  model: function(){
-    return '';
-  }
-});
-  
-
-
-
 App.ApplicationAdapter = DS.FirebaseAdapter.extend({
   firebase: new Firebase("https://grapevinecom.firebaseio.com/")
 });
@@ -40,74 +29,197 @@ App.ApplicationAdapter = DS.FirebaseAdapter.extend({
 
 App.Post = DS.Model.extend({
   title: DS.attr(''),
-  uid: DS.attr(''),
-  user: DS.attr(''),
-  created: DS.attr(''),
-  updated: DS.attr(''),
-  content: DS.attr(''),
-  tags: DS.attr(''),
-  location: DS.attr(''),
-  likes: DS.attr(''),
-  views: DS.attr(''),
+  // content: DS.attr(''),
+  // created: DS.attr(''),
+  // updated: DS.attr(''),
+  // // uid: DS.attr(''),
+  // // user: DS.attr(''),
+  // tags: DS.attr('', {defaultValue: ''}),
+  // location: DS.attr('', {defaultValue: ''}),
+  // likes: DS.attr('', {defaultValue: 0}),
+  // views: DS.attr('', {defaultValue: 0}),
   issue: DS.belongsTo('issue', { async: true })
 });
 
 App.Issue = DS.Model.extend({
-  title: DS.attr(),
-  created: DS.attr(''),
-  updated: DS.attr(''),
-  uid: DS.attr(''),
-  user: DS.attr(''),
-  posts: DS.hasMany('post', { async: true })
+  title: DS.attr(''),
+  // content: DS.attr('', {defaultValue: ''}),
+  // created: DS.attr(''),
+  // updated: DS.attr(''),
+  // uid: DS.attr(''),
+  // user: DS.attr(''),
+  posts: DS.hasMany('post', { asyn: true }) // misspelled on purpose!!!!! HACK TO-DO
+});
+
+App.Router.map(function() {
+    this.route('index', {path: "/"});
+
+    this.route('login');
+    this.route('signup');
+
+    this.route('issue', {path: "issue/:issueID"})
+    this.route('newpost', {path: "issue/newpost/:issueID"});
+    this.route('newissue');
+});
+
+//for User App
+App.ApplicationRoute = Ember.Route.extend(Ember.UserApp.ApplicationRouteMixin);
+App.SignupController = Ember.Controller.extend(Ember.UserApp.FormControllerMixin);
+App.LoginController = Ember.Controller.extend(Ember.UserApp.FormControllerMixin);
+App.IndexRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin, {
+  model: function(){
+    return this.store.find('issue');
+  }
+});
+  
+App.IssueRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin, {
+  model: function(params){
+    return this.store.find('issue', params.issueID);
+  }
+});
+App.NewissueController = Ember.Controller.extend({
+    title: "issue title",
+    actions: {
+      createIssue: function(){
+        
+          var _this = this;
+          
+          // Create new Issue
+          var newIssue = _this.store.createRecord('issue', {
+            title: _this.get('title'),
+            created: moment().unix(),
+            updated: moment().unix(),
+            uid: _this.get('user').current.user_id,
+            user: _this.get('user').current
+          });
+
+          // Save it to firebase
+          newIssue = newIssue.save();
+
+          // Check if succesfull :)
+          newIssue.then(function(a){
+            alert('success')
+          }, function(a){
+            debugger;
+            alert('failure');
+          })
+
+      }
+    }
+})
+
+
+
+App.GoogleMapsComponent = Ember.Component.extend({
+  insertMap: function() {
+    var container = this.$('.map-canvas');
+
+    var options = {
+      center: new google.maps.LatLng(this.get('latitude'), this.get('longitude')),
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.set('map', new google.maps.Map(container[0], options));
+    this.set('markerCache', []);
+
+    this.setMarkers();
+  }.on('didInsertElement'),
+  
+  coordinatesChanged: function() {
+    var map = this.get('map');
+
+    if (map) map.setCenter(new google.maps.LatLng(this.get('latitude'), this.get('longitude')));
+  }.observes('latitude', 'longitude'),
+  
+  setMarkers: function() {
+    var map = this.get('map'),
+        markers = this.get('markers'),
+        markerCache = this.get('markerCache');
+
+    if (markers){
+      markerCache.forEach(function(marker) { marker.setMap(null); }); // Remove all existing markers
+
+      markers.forEach(function(marker){
+        var gMapsMarker = new google.maps.Marker({
+          position: new google.maps.LatLng(marker.get('latitude'), marker.get('longitude')),
+          map: map
+        });
+
+        markerCache.pushObject(gMapsMarker); // Add this marker to our cache
+      }, this);
+    }
+  }.observes('markers.@each.latitude', 'markers.@each.longitude')
 });
 
 
-    //   $(window).resize(function () {
-    //       var h = $(window).height(),
-    //         offsetTop = 150; // Calculate the top offset
-		
-    
-    //       $('#map_canvas').css('height', (h - offsetTop ));
-		  // $('#content').css('height', (h - offsetTop + 18));
-		  // $('#subcontent').css('height', (h - offsetTop + 12));
-    //     }).resize();
-        
-    //     $(function() {
-    //       MapsLib.initialize();
-    //       $("#search_address").geocomplete();
 
-    //       $(':checkbox').click(function(){
-    //         MapsLib.doSearch();
-    //       });
 
-    //       $(':radio').click(function(){
-    //         MapsLib.doSearch();
-    //       });
-          
-    //       $('#search_radius').change(function(){
-    //         MapsLib.doSearch();
-    //       });
-          
-    //       $('#search').click(function(){
-    //         MapsLib.doSearch();
-    //       });
-          
-    //       $('#find_me').click(function(){
-    //         MapsLib.findMe(); 
-    //         return false;
-    //       });
-          
-    //       $('#reset').click(function(){
-    //         $.address.parameter('address','');
-    //         MapsLib.initialize(); 
-    //         return false;
-    //       });
-          
-    //       $(":text").keydown(function(e){
-    //           var key =  e.keyCode ? e.keyCode : e.which;
-    //           if(key == 13) {
-    //               $('#search').click();
-    //               return false;
-    //           }
-    //       });
-    //     });
+App.NewpostRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin, {
+  model: function(params){
+    return this.store.find('issue', params.issueID);
+  }
+});
+App.NewpostController = Ember.Controller.extend({
+    postTitle: "enter title here",
+    postContent: "enter content here",
+    actions: {
+      createPost: function(){
+          var _this = this;
+
+          this.store.find('issue', this.get('model.id')).then(function(issue){
+
+            debugger;
+
+            //create new Post
+            var newPost = _this.store.createRecord('post', {
+                title: _this.get('postTitle'),
+                issue: issue
+                // content: _this.get('postContent'),
+                // created: moment().unix(),
+                // updated: moment().unix(),
+                // uid: _this.get('user').current.user_id,
+                // user: _this.get('user')
+            });
+
+            //save it to firebase
+            var newPostPromise = newPost.save();
+            // newPost.save();
+            
+            //add to the issue
+
+            newPostPromise.then(function(){
+
+              issue.get('posts').addObject(newPost);
+              var modelPromise = issue.save()
+
+              modelPromise.then(function(){
+                debugger;
+              }, function(argument) {
+                debugger;
+                
+              });
+            })
+
+          })
+          //this.get('model') is the issue
+          // debugger;
+
+          // If successful, add to issue and save issue
+          //   newPostPromise.then(function(a){
+          //     alert('success')
+          //   }, function(a){
+          //     alert('failure');
+          //   })
+
+          // // Check if succesfull :)
+          // modelPromise.then(function(a){
+          //   alert('success')
+          // }, function(a){
+          //   alert('failure');
+          // })
+      }
+    }
+})
+
+});
